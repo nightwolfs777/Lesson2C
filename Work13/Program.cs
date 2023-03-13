52c6f763237d386303e04704a64f1ab7581ae26c
IldarPC <Ildarka.201995@gmail.com>
2023-03-13 16:35:11 +0500

    Work 13

--- Work13/Program.cs
+++ Work13/Program.cs
@@ -6,4 +6,20 @@
 
 // 32679 -> 6
 
+int num;
+Console.WriteLine($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out num);
 
+if (num >= 100)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    int a1 = num % 10;
+    Console.WriteLine($"{num} -> {a1}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
