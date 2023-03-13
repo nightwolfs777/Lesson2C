042efd4c73f2ba34aa27ca92b24034ad65344ccb
IldarPC <Ildarka.201995@gmail.com>
2023-03-13 13:23:29 +0500

    Work10

--- Work10/Program.cs
+++ Work10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num;
+Console.WriteLine($"Введите трехзначное число: ");
+int.TryParse(Console.ReadLine()!, out num);
+
+int a = num % 100;
+Console.WriteLine($"{num} -> {a / 10}");
